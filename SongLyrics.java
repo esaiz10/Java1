93a9c8568a20be10e28f2efb5bfb75c151a0452a
Ernesto Saiz <165823215+esaiz10@users.noreply.github.com>
2025-01-19 22:48:57 -0500

    Add files via upload

---
+++ SongLyrics.java
@@ -0,0 +1,8 @@
+public class SongLyrics {
+    public static void main(String[] args) {
+        System.out.println("I'm a lion tamer");
+        System.out.println("I'm a lion tamer");
+        System.out.println("I'm a lion tamer");
+        System.out.println("I'm a lion tamer");
+    }
+}
---
+++ Triangle.java
@@ -0,0 +1,9 @@
+public class Triangle {
+    public static void main(String[] args) {
+        System.out.println("     T");
+        System.out.println("    TTT");
+        System.out.println("   TTTTTT");
+        System.out.println(" TTTTTTTTTT");
+        System.out.println("TTTTTTTTTTTTT");
+    }
+}
