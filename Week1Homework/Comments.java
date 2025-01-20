7a8b3853c2e48ccd97f16e64e9b7b375719bb633
Ernesto Saiz <165823215+esaiz10@users.noreply.github.com>
2025-01-19 23:07:06 -0500

    Add files via upload

---
+++ Week1Homework/Comments.java
@@ -0,0 +1,19 @@
+public class Comments {
+    public static void main(String[] args) {
+        // Display the statement about comments
+        System.out.println("Program comments are nonexecuting statements you add to a file for documentation.");
+
+        // Single-line comment: This is a single-line comment
+        // Program comments are nonexecuting statements you add to a file for documentation.
+
+        /*
+         * Multi-line comment: This is a multi-line comment
+         * Program comments are nonexecuting statements you add to a file for documentation.
+         */
+
+        /**
+         * Documentation comment: This is a documentation comment
+         * Program comments are nonexecuting statements you add to a file for documentation.
+         */
+    }
+}
---
+++ Week1Homework/MovieQuoteInfo.java
@@ -0,0 +1,8 @@
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+        System.out.println("Why so serious?");
+        System.out.println("- The Dark Knight");
+        System.out.println("- The Joker (Heath Ledger)");
+        System.out.println("- 2008");
+    }
+}
